ffa2b5fbc3e08e567ef5cca4c44dd8a3c1b7a00b
Dominik <dominik.finkbeiner@sap.com>
2016-04-09 18:04:12 +0200

    add fps

--- phasergame.js
+++ phasergame.js
@@ -1,7 +1,7 @@
-var game = new Phaser.Game(1280 , 720, Phaser.CANVAS, 'phaser-example', { preload: preload, create: create });
+var game = new Phaser.Game(1280, 720, Phaser.CANVAS, 'phaser-example', { preload: preload, create: create, render: render });
 
 function preload() {
-
+    game.time.advancedTiming = true;
     game.load.image('ball', 'pangball.png');
 
 }
@@ -18,11 +18,14 @@ function create() {
 
     var group = game.add.physicsGroup(Phaser.Physics.P2JS);
 
-    for (var i = 0; i < 70; i++)
-    {
+    for (var i = 0; i < 70; i++) {
         var ball = group.create(game.world.randomX, game.rnd.between(0, 100), 'ball');
         ball.body.setCircle(16);
         ball.body.fixedRotation = true;
     }
 
+}
+
+function render() {
+    game.debug.text(game.time.fps || '--', 2, 14, "#00ff00");
 }
